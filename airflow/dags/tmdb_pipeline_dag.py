1a47d9bd9cd263e35cb5f4dea5a819f5f0bf2afd
Florencia Gon <florencia.gbog@gmail.com>
2026-04-01 09:07:06 +0200

    Clean up DAG formatting

--- airflow/dags/tmdb_pipeline_dag.py
+++ airflow/dags/tmdb_pipeline_dag.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     dag_id="tmdb_movie_pipeline",
     start_date=datetime(2026, 3, 14),
-    schedule= None ,
+    schedule= None,
     catchup=False,
 ) as dag:
 
